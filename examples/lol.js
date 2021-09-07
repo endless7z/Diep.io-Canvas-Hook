4513cd7d111990cd771725dd2167ebf7f905146f
endless <73157473+endlessXD@users.noreply.github.com>
2021-09-06 22:56:39 -0700

    ye

--- examples/lol.js
+++ examples/lol.js
@@ -1,5 +1,5 @@
 const Canvas = new CanvasHook();
 
 Canvas.on(['fillText', 'strokeText'], text => {
-    text.element.arguments[0] = 'lol'; // replaces all text with "lol"
+    text.arguments[0] = 'lol'; // replaces all text with "lol"
 });
